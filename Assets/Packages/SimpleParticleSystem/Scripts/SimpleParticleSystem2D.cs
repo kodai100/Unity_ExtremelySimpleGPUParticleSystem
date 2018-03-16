1f3326b421244273c231f04d8cd2a69046f321e6
Takao Kodai (Lab Desktop) <takaokodai@team-lab,com>
2018-03-16 22:17:18 +0900

    Initial Commit

---
+++ Assets/Packages/SimpleParticleSystem/Scripts/SimpleParticleSystem2D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace SimpleParticleSystem
+{
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct ParticleData2D
+    {
+        [FieldOffset(0)]
+        public bool isActive;
+        [FieldOffset(4)]
+        public int id;
+        [FieldOffset(8)]
+        public Vector2 position;
+    }
+
+    public class SimpleParticleSystem2D : SimpleParticleSystemBase<ParticleData2D>
+    {
+
+        [SerializeField] ComputeShader _particleCS;
+
+        protected override void InitParticles()
+        {
+            int kernel = _particleCS.FindKernel("Init");
+            _particleCS.SetBuffer(kernel, "_ParticlesBuffer", ParticleBuffer);
+            _particleCS.Dispatch(kernel, ParticleNum / THREAD_NUM_X, 1, 1);
+        }
+
+        protected override void UpdateParticles()
+        {
+            _particleCS.SetFloat("_TimeSinceStartup", Time.realtimeSinceStartup);
+
+            int kernel = _particleCS.FindKernel("Update");
+            _particleCS.SetBuffer(kernel, "_ParticlesBuffer", ParticleBuffer);
+            _particleCS.Dispatch(kernel, ParticleNum / THREAD_NUM_X, 1, 1);
+
+        }
+
+    }
+}
---
+++ Assets/Packages/SimpleParticleSystem/Scripts/SimpleParticleSystemBase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace SimpleParticleSystem
+{
+
+    public enum ParticleNumEnum
+    {
+        NUM_8K = 8 * 1024,
+        NUM_16K = 16 * 1024,
+        NUM_32K = 32 * 1024,
+        NUM_64K = 64 * 1024
+    }
+
+
+    public abstract class SimpleParticleSystemBase<T> : MonoBehaviour where T : struct
+    {
+
+        protected const int THREAD_NUM_X = 1024;
+
+
+        [SerializeField] ParticleNumEnum _particleNumEnum = ParticleNumEnum.NUM_8K;
+        [SerializeField] Material _particleRenderMaterial;
+
+        private int _particleNum;
+        private ComputeBuffer _particleBuffer;
+
+        public int ParticleNum { get { return _particleNum; } }
+        public ComputeBuffer ParticleBuffer { get { return _particleBuffer; } }
+
+        void Start()
+        {
+            _particleNum = (int)_particleNumEnum;
+            _particleBuffer = new ComputeBuffer(_particleNum, Marshal.SizeOf(typeof(T)));
+            InitParticles();
+        }
+        
+        void Update()
+        {
+            UpdateParticles();
+        }
+        
+
+        protected abstract void InitParticles();
+        protected abstract void UpdateParticles();
+        
+
+        private void OnRenderObject()
+        {
+            _particleRenderMaterial.SetPass(0); // !important
+            _particleRenderMaterial.SetBuffer("_ParticlesBuffer", _particleBuffer);
+            Graphics.DrawProcedural(MeshTopology.Points, _particleNum);
+        }
+
+        private void OnDestroy()
+        {
+            _particleBuffer.Release();
+        }
+    }
+}
+
+
